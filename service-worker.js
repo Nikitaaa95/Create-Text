1e6d63429f5310c15bac2c2c09e54d6d121356dc
Nikita Thomas <nikitathomas@Nikitas-MBP.lan>
2024-03-23 16:53:30 -0400

    service-worker.js update to creat dist file

---
+++ service-worker.js
@@ -0,0 +1,73 @@
+// Define the cache name
+const CACHE_NAME = 'jate-cache-v1';
+
+// List of files to cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/install.html',
+  '/assets/icons/icon_96x96.png',
+  '/main.bundle.js',
+];
+
+// Install the service worker and cache the assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached assets when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request to make it safe to use
+        const fetchRequest = event.request.clone();
+
+        // If request is not cached, fetch and cache it
+        return fetch(fetchRequest).then(
+          (response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response to save a copy in the cache
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
+// Remove old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName !== CACHE_NAME;
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
