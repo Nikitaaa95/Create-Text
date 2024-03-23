e187862cbe78c83435f9110c05156a988c3e8db0
Nikita Thomas <nikitathomas@Nikitas-MBP.lan>
2024-03-23 17:13:24 -0400

    ReadMe set up

---
+++ readME.md
@@ -0,0 +1,6 @@
+Package.json documentation for troubleshooting:
+https://phoenixnap.com/kb/package-json
+
+How to get dist folder together:
+https://stackoverflow.com/questions/39729194/role-of-the-src-and-dist-folders-in-npm-packages
+
--- server/server.js
+++ server/server.js
@@ -8,6 +8,6 @@ app.use(express.static(path.join(__dirname, 'client/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./server/routes/htmlRoutes')(app);
+require('../server/routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
